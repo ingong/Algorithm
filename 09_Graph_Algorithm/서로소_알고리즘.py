deb24eda29a6d5792dfe226c6a144738c3ef56d6
ingong <insong1216@gmail.com>
2021-10-29 22:32:39 +0900

    그래프 알고리즘: 서로소 알고리즘

---
+++ 09_Graph_Algorithm/서로소_알고리즘.py
@@ -0,0 +1,25 @@
+def find_parent(parent, x):
+  if x != parent[x]:
+    find_parent(parent, parent[x])
+  return parent[x]
+
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
+    
+v, e = map(int, input().split())
+parent = [i for i in range(v + 1)]
+
+for i in range(e):
+  a, b = map(int, input().split())
+  union_parent(parent, a, b)
+
+for i in range(1, v + 1):
+  print(find_parent(parent, i), end='')
+
+for i in range(1, v + 1):
+  print(parent[i], end = '')
