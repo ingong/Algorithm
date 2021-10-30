7553ec05caba294e6c3ca4a471892775260713da
ingong <insong1216@gmail.com>
2021-10-31 01:20:17 +0900

    211031 1solved

---
+++ 12_Tree/PG디스크컨트롤러.py
@@ -0,0 +1,17 @@
+import heapq
+from collections import deque
+
+def solution(jobs):
+    tasks = deque(sorted([(x[1], x[0]) for x in jobs], key=lambda x: (x[1], x[0])))
+    q = []
+    heapq.heappush(q, tasks.popleft())
+    current_time, total_time = 0, 0
+    while len(q) > 0:
+        spend, request = heapq.heappop(q)
+        current_time = max(current_time + spend, request + spend)
+        total_time += (current_time - request)
+        while len(tasks) > 0 and tasks[0][1] <= current_time:
+            heapq.heappush(q, tasks.popleft())
+        if len(tasks) > 0 and len(q) == 0:
+            heapq.heappush(q, tasks.popleft())
+    return total_time // len(jobs)
