77e477060ed943c0e5914ffaa90af74aee569ad1
ingong <insong1216@gmail.com>
2021-11-13 15:27:24 +0900

    211113: 1sovled

--- ETC/Test.py
+++ ETC/Test.py
@@ -11,27 +11,23 @@ distance = [INF] * (n + 1)
 for _ in range(m):
   a, b, c = map(int, input().split())
   graph[a].append((b, c))
-
+  
 def dijkstra(start):
   q = []
   heapq.heappush(q, (0, start))
   distance[start] = 0
   while q:
-    dist, node = heapq.heappop(q)
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+      continue
     
-    if distance[node] < dist:
-        continue
-      
-    for i in graph[node]:
+    for i in graph[now]:
       cost = dist + i[1]
+      
       if cost < distance[i[0]]:
-        heapq.heappush(q, (cost, i[0]))
         distance[i[0]] = cost
+        heapq.heappush(q, (cost, i[0]))
+
 
 dijkstra(start)
-
-for i in range(1, n + 1):
-  if distance[i] == INF:
-    print('INFINITY')
-  else:
-    print(distance[i])
+print(distance)
